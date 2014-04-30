2fe4d158eb2db549f5fe71b76daae35afd7b9727
Jordan Bayles <baylesj@onid.orst.edu>
2014-04-30 09:40:59 -0700

    Working on proper output

--- 2/doit.sh
+++ 2/doit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Jordan Bayles
 # VLSI HW #2
 # 4/30/2014
@@ -19,15 +19,9 @@ if [ -s "adder.do" ] ; then
 	vsim alu -do alu.do -quiet -c -t 1ps
 fi
 
-
 # synthesize alu
 if [ -s "syn_alu" ] ; then
 	dc_shell-xg-t -f syn_alu
-fi
-
-# compile the gate library if it hasn't been done yet
-if [ -s `grep alu work/_info/*` ] ; then
-
 fi
 
 # compile the .gate.v file
@@ -35,6 +29,5 @@ if [ -s "alu.gate.v" ] then
 	vlog -novopt alu.gate.v
 fi
 
-# second run of the simulation
 vsim alu -do alu.do -quiet -c -t 1ps
 
