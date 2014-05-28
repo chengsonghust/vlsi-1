8a74d957553c5deb620661fbe2465a13ade7e71f
Jordan Bayles (baylesj) <bayles.jordan@gmail.com>
2014-05-28 09:23:42 -0700

    Finished golden c code (perhaps except formatting)

--- 5/gcd.c
+++ 5/gcd.c
@@ -9,11 +9,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define is_even(a) (((a) & 1) == 0)
+#define INPUT "./vectors/input_data"
+#define OUTPUT "./vectors/golden_data"
 
-unsigned int gcd(unsigned int a, unsigned int b)
+#define is_even(a) (((a) & 1) == 0)
+#define array_len(a) ((sizeof(a))/(sizeof((a)[0])))
+
+struct data {
+	unsigned int a;
+	unsigned int b;
+	unsigned int gcd;
+};
+
+unsigned int find_gcd(struct data *in)
 {
 	int shift;
+	unsigned int a;
+	unsigned int b;
+
+	a = in->a;
+	b = in->b;
 
 	// base cases gcd(0, 0) = 0, gcd(a, 0) = a, gcd(a, b) = b
 	if (a == 0) {
@@ -52,11 +67,39 @@ unsigned int gcd(unsigned int a, unsigned int b)
 	}
 
 	// shift by shared factors of two
-	return a << shift;
+	in->gcd = a << shift;
+	return in->gcd;
+}
+
+void stream_gcd()
+{
+	FILE *in;
+	FILE *out;
+
+	in = fopen(INPUT, "r");
+	if (in == NULL) {
+		return;
+	}
+
+	out = fopen(OUTPUT, "w");
+	if (out == NULL) {
+		return;
+	}
+
+	struct data cur;
+	while (fscanf(in, "%d %d", &cur.a, &cur.b) > 0) {
+		find_gcd(&cur);
+		printf("%10d, %10d -> %10d\n", cur.a, cur.b, cur.gcd);
+		fprintf(out, "%d\n", cur.gcd);
+	}
+
+	fclose(in);
+	fclose(out);
 }
 
 int main(int argc, char **argv)
 {
-	printf("%d, %d -> %u\n", 10, 20, gcd(10, 20));
+	stream_gcd();
 
+	return 0;
 }
